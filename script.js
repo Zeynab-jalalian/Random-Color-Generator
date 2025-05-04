e3cb8af9aad4f6a0928a83ac92dd2016860ff7e9
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-05-04 09:41:38 +0330

    script 2

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-let box = document.querySelector(".box");
+let box=document.querySelector(".box");
 
-function colorChange(color) {
-   let codeDiv = document.createElement("div");
-    codeDiv.classList.add("codeStyle");
-    codeDiv.appendChild(document.createTextNode(color));
-   
+function colorChange(color){
+    let codeColorDiv=document.createElement("div");
+    codeColorDiv.classList.add("codeStyle");
+    codeColorDiv.appendChild(document.createTextNode(color));
+    
     if(box.hasChildNodes){
         box.innerHTML='';
     }
+   let span= document.querySelectorAll("span");
+   span.forEach(function(item){
+    item.classList.remove("active");
+   })
+   event.target.classList.add("active");
 
     box.style.background=color;
-    box.appendChild(codeDiv);
-
+    box.appendChild(codeColorDiv);
 }
